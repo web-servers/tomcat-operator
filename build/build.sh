d687a57e8f90348257828daa70db6088dc10a412
root <root@neo6.gva.redhat.com>
2019-10-02 13:28:17 +0200

    Add support for arm64 note the ubi8 has arm64 but not ubi7.

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+ARCH=amd64
+UNAMEARCH=`uname -p`
+if [ $UNAMEARCH=aarch64 ]; then
+  ARCH=arm64
+fi
+echo "using ARCH: ${ARCH}"
+
 GOOS=${1-linux}
 BIN_DIR=${2-$(pwd)/build/_output/bin}
 mkdir -p ${BIN_DIR}
@@ -13,4 +20,4 @@ BUILD_PATH="${REPO_PATH}/cmd/manager"
 VERSION="$(git describe --tags --always --dirty)"
 GO_LDFLAGS="-X ${REPO_PATH}/version.Version=${VERSION}"
 echo "building ${PROJECT_NAME}..."
-GOOS=${GOOS} GOARCH=amd64 CGO_ENABLED=0 go build -o ${BIN_DIR}/${PROJECT_NAME} -ldflags "${GO_LDFLAGS}" $BUILD_PATH
+GOOS=${GOOS} GOARCH=${ARCH} CGO_ENABLED=0 go build -o ${BIN_DIR}/${PROJECT_NAME} -ldflags "${GO_LDFLAGS}" $BUILD_PATH
